9a3d499cf5f454cd4f1115b7f728e719b8134134
Matthew Stafford <matthew.stafford@bluelabs.com>
2019-02-06 20:52:18 -0500

    Hardcode version bump

--- gremlin-python/src/main/jython/setup.py
+++ gremlin-python/src/main/jython/setup.py
@@ -55,7 +55,7 @@ if sys.version_info < (3,2):
 
 setup(
     name='gremlinpython',
-    version=version,
+    version=3.3.6,
     packages=['gremlin_python', 'gremlin_python.driver',
               'gremlin_python.driver.tornado', 'gremlin_python.process',
               'gremlin_python.structure', 'gremlin_python.structure.io'],
